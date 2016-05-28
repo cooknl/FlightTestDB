0245d438a8b9cd045ddf913e3e151af37b3e9fa3
Nate <nathan.gs20@gmail.com>
2016-05-28 00:40:46 -0500

    Migrated test_results
    New table

--- flight_test_db/test_results/models.py
+++ flight_test_db/test_results/models.py
@@ -1,7 +1,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
 # Create your models here.
 class TestResult(models.Model):
-    location = models.CharField(
+    short_title = models.SlugField()
+    location_area = models.CharField(max_length=30)
+    location_lat_long = gis_models.PointField(geography=True)
+    vehicle = models.CharField(max_length=30)
+    operator = models.CharField(max_length=30)
+    flight_date = models.DateField()
+    result_start_time = models.TimeField()
+    result_stop_time = models.TimeField()
+    test_success = models.CharField(max_length=30)
+    system_evaluation = models.CharField(max_length=30)
+    notes = models.TextField()
+    result_filepath = models.FilePathField()
+
+
+    def __unicode__(self):
+        return u'%s %s' % (self.flight_date, self.short_title)
